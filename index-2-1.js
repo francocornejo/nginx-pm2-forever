50dda6a762f3c7f65d19b8ac39c79c5d47cb72f7
francocornejo <franco-cornejo@hotmail.com>
2022-09-01 17:25:00 -0300

    modificacion index2-1.js

---
+++ index-2-1.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cluster = require("cluster");
+const os = require("os");
+const rutas = require("./routes/index.js");
+
+const app = express();
+const cpus = os.cpus();
+const PORT = Number(process.argv[2]) || 3000;
+const iscluster = process.argv[3] == "cluster";
+
+if (iscluster && cluster.isPrimary) {
+  cpus.map(() => {
+    cluster.fork();
+  });
+} else {
+  app.get("/api/randoms/datos", (req, res) => {
+    res.send("Server2-2");
+  });
+
+  app.listen(PORT, () => {
+    console.log("Server listening port 3000");
+  });
+}
